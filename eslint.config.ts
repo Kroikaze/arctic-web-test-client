0250bf890702fecdf7aa258ede35fe74a9208e44
Oleh Khrystenko <olegkhristenko@gmail.com>
2025-03-04 10:52:20 +0200

    installed @typescript-eslint/eslint-plugin, @typescript-eslint/parser, eslint-config-prettier, eslint-plugin-prettier, eslint-plugin-react and prettier; setting eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,47 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import type { Linter } from 'eslint';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+
+const compat = new FlatCompat();
+
+export default [
+    { ignores: ['dist', '**/*.d.ts'] },
+    js.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2024,
+            sourceType: 'module',
+            globals: globals.browser,
+            parser: tsParser,
+            parserOptions: {
+                project: './tsconfig.app.json',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint as unknown as Linter,
+            'react': reactPlugin,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'prettier/prettier': 'error',
+            ...reactPlugin.configs.recommended.rules,
+        },
+    },
+    ...compat.extends('plugin:react/recommended', 'plugin:prettier/recommended')
+] satisfies Linter.Config[];
---
+++ types.d.ts
@@ -0,0 +1,6 @@
+declare module 'eslint-plugin-react-hooks' {
+    import { Linter } from 'eslint';
+
+    const plugin: Linter;
+    export default plugin;
+}
