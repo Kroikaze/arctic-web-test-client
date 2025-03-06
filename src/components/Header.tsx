9121bdd83341862c23227a2e3c50ab393b10edaf
Kroikaze <Kroikaze@gmail.com>
2025-03-06 12:14:01 +0200

    Logo to component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,15 @@
 import { Link } from 'react-router-dom';
 
-import ShoppingCart from './ShoppingCart.tsx';
+import Logo from './Logo.tsx';
 import Nav from './Nav.tsx';
+import ShoppingCart from './ShoppingCart.tsx';
 
 const Header = () => {
     return (
         <header className="bg-white">
             <div className="container mx-auto flex justify-between items-center py-4 px-6">
                 <ShoppingCart />
-                <div className="flex flex-col text-center">
-                    <h1 className="text-4xl">NEVA TIME</h1>
-                    <p className="text-sm">
-                        Фирменные аксессуары Michael Kors
-                        <br />с доставкой
-                    </p>
-                </div>
+                <Logo />
                 <div className="flex flex-col text-center">
                     <span className="font-semibold text-2xl">8 (800) 302 79 15</span>
                     <Link to="/callback" className="text-sm underline text-[#CDB494]">
---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+    return (
+        <div className="flex flex-col text-center">
+            <h1 className="text-4xl">NEVA TIME</h1>
+            <p className="text-sm">
+                Фирменные аксессуары Michael Kors
+                <br />с доставкой
+            </p>
+        </div>
+    );
+};
+
+export default Logo;
