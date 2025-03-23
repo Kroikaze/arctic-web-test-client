9c8bd99b4869beed6ed0b6b2ea6aaf54236135f4
Kroikaze <Kroikaze@gmail.com>
2025-03-23 17:16:00 +0200

    size to burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
     return (
         <header className="bg-white">
-            <div className="container mx-auto flex justify-between items-center px-6 py-4">
+            <div className="mx-auto flex justify-between items-center px-6 py-4">
                 <div className="order-1 block xl:hidden">
                     <button onClick={toggleMobileMenu} className="text-2xl">
                         ☰
