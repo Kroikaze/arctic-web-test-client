40eea4d32d180a6284c7812dc844889ab764f499
Kroikaze <Kroikaze@gmail.com>
2025-04-01 13:32:49 +0300

    created EmailSubscribe

---
+++ src/components/Footer/EmailSubscribe.tsx
@@ -0,0 +1,42 @@
+import { FC, FormEvent, useState } from 'react';
+
+const EmailSubscribe: FC = () => {
+    const [email, setEmail] = useState('');
+    const [isSubscribed, setIsSubscribed] = useState(false);
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log('Email submitted:', email);
+        setIsSubscribed(true);
+        setEmail('');
+    };
+
+    return (
+        <div className="mt-4 text-white">
+            <p className="text-sm mb-2">Подпишись на новинки и скидки</p>
+
+            {isSubscribed ? (
+                <p className="text-sm text-latte">Спасибо за подписку!</p>
+            ) : (
+                <form onSubmit={handleSubmit} className="">
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="E-mail"
+                        className="bg-white text-gray-400 px-3 py-2 text-sm w-1/2"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="bg-latte text-white px-4 py-2 text-sm font-medium mb-4 w-1/2"
+                    >
+                        ОТПРАВИТЬ
+                    </button>
+                </form>
+            )}
+        </div>
+    );
+};
+
+export default EmailSubscribe;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,16 @@ import { FC } from 'react';
 
 import Logo from '@/components/Header/Logo.tsx';
 
+import EmailSubscribe from './EmailSubscribe';
+
 const Footer: FC = () => {
     return (
         <div className="bg-black">
             <div className="container mx-auto flex justify-between ">
-                <Logo externalClasses="text-white text-left" />
+                <div className="flex-col">
+                    <Logo externalClasses="text-white text-left font-light" />
+                    <EmailSubscribe />
+                </div>
             </div>
         </div>
     );
