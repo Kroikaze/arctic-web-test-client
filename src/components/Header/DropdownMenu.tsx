012cb34d2afd2c56f720776ca4646277d0c87b97
Kroikaze <Kroikaze@gmail.com>
2025-04-03 12:16:21 +0300

    w-full for dropdown menu in mobile

--- src/components/Header/DropdownMenu.tsx
+++ src/components/Header/DropdownMenu.tsx
@@ -8,13 +8,13 @@ interface DropdownMenuProps {
 
 const DropdownMenu: FC<DropdownMenuProps> = ({ items, callback }) => {
     return (
-        <div className="absolute -left-1 w-36 mt-4 bg-latte animate-fade-in">
+        <div className="absolute -left-1 w-36 mt-4 bg-latte animate-fade-in max-laptop-sm:w-full">
             <div className="absolute -top-2 left-1/2 -translate-x-1/2 rotate-45 w-4 h-4 bg-latte"></div>
             {items.map(item => (
                 <Link
                     key={item.text}
                     to={item.link}
-                    className="block capitalize px-4 py-2 text-sm text-white"
+                    className="block capitalize px-4 py-2 text-sm text-white max-laptop-sm:text-center"
                     onClick={callback}
                 >
                     {item.text}
