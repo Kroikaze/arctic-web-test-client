a7dc58908dec15237f3693d1557dceb0da3a248b
Kroikaze <Kroikaze@gmail.com>
2025-04-22 11:26:48 +0300

    Portal 2

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { Link } from 'react-router-dom';
 
@@ -15,12 +15,39 @@ interface NavProps {
 const Nav: FC<NavProps> = ({ isMobileOpen = false }) => {
     const { BigScreen } = useScreenSize();
     const [isDropdownOpen, setDropdownOpen] = useState<boolean>(false);
+    const divRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
-    const portalPosition = {
-        top: 100,
-        left: 100,
-        width: 200,
-    };
+    const [portalPosition, setPortalPosition] = useState({ top: 0, left: 0, width: 0 });
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (divRef.current) {
+                const rect = divRef.current.getBoundingClientRect();
+                console.log('Координати:', {
+                    top: rect.top,
+                    left: rect.left,
+                    width: rect.width,
+                });
+                setPortalPosition({
+                    top: rect.bottom + window.scrollY,
+                    left: rect.left + window.scrollX,
+                    width: rect.width,
+                });
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        const container = containerRef.current;
+        if (container) {
+            container.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            container?.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const portalDropdown = (
         <div
@@ -53,6 +80,7 @@ const Nav: FC<NavProps> = ({ isMobileOpen = false }) => {
                 `}
         >
             <div
+                ref={containerRef}
                 className="container mx-auto flex flex-col items-start  gap-1
                     laptop-sm:flex-row
                     laptop-sm:items-center
@@ -64,6 +92,7 @@ const Nav: FC<NavProps> = ({ isMobileOpen = false }) => {
                     if (nav.link === undefined) {
                         return (
                             <div
+                                ref={divRef}
                                 key={nav.text}
                                 className="relative uppercase p-4 cursor-pointer max-laptop-sm:w-full group"
                                 onClick={() => !BigScreen && setDropdownOpen(!isDropdownOpen)}
@@ -76,16 +105,18 @@ const Nav: FC<NavProps> = ({ isMobileOpen = false }) => {
                                         className="w-2 h-1 ml-2 duration-150 group-hover:rotate-0 rotate-180"
                                     />
                                 </div>
-                                <DropdownMenu
-                                    items={[
-                                        { text: 'Женские часы', link: '/watches/women' },
-                                        { text: 'Мужские часы', link: '/watches/men' },
-                                    ]}
-                                    className={`
+                                {BigScreen && (
+                                    <DropdownMenu
+                                        items={[
+                                            { text: 'Женские часы', link: '/watches/women' },
+                                            { text: 'Мужские часы', link: '/watches/men' },
+                                        ]}
+                                        className={`
                                               ${BigScreen ? 'hidden group-hover:block' : ''} 
                                               ${isDropdownOpen ? 'block' : 'hidden'}
                                             `}
-                                />
+                                    />
+                                )}
                                 {!BigScreen &&
                                     isDropdownOpen &&
                                     createPortal(portalDropdown, document.body)}
